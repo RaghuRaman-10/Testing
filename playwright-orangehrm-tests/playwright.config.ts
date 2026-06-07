c1eb940e73928d38f8f9d12cc7cedf0a9bd627c4
RaghuRaman-10 <raghu130900@gmail.com>
2026-06-07 18:23:31 +0530

    Chore: Initial project Setup

---
+++ playwright-orangehrm-tests/playwright.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  use: {
+    baseURL: 'https://opensource-demo.orangehrmlive.com',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  reporter: [['list'], ['allure-playwright']],
+});
